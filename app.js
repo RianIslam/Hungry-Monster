0b73dde0067febbc7effb51ab4c1836c5b5a803c
RianIslam <rianislamdr@gmail.com>
2021-02-07 22:15:17 +0600

    Alhamdulillah done

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const button = document.getElementById("mealBtn");
 const inputValue = document.getElementById("inputMeal");
+
 button.addEventListener("click", function () {
   fetch(
     "https://www.themealdb.com/api/json/v1/1/search.php?s=" +
-      inputValue.value +
+    inputValue.value +
       ""
   )
     .then((res) => res.json())
     .then((data) => displayfoods(data));
 });
+
+
 
 const displayfoods = (meal) => {
   const mealItem = meal.meals;
@@ -20,8 +23,8 @@ const displayfoods = (meal) => {
 
     const mealDiv = document.createElement("div");
     const mealInfo = `
-    <img onclick="displayFood('<img src=${food.strMealThumb}><h1>${food.strMeal}</h1><li>${food.strIngredient1}</li><li>${food.strIngredient2}</li><li>${food.strIngredient3}</li><li>${food.strIngredient4}</li><li>${food.strIngredient5}</li><li>${food.strIngredient6}</li><li>${food.strIngredient7}</li> <li>${food.strIngredient8}</li> <li>${food.strIngredient9}</li> <li>${food.strIngredient10}</li>')" src=${food.strMealThumb}>
-    <h4 onclick="displayFood('<img src=${food.strMealThumb}><h1>${food.strMeal}</h1><li>${food.strIngredient1}</li><li>${food.strIngredient2}</li><li>${food.strIngredient3}</li><li>${food.strIngredient4}</li><li>${food.strIngredient5}</li><li>${food.strIngredient6}</li><li>${food.strIngredient7}</li> <li>${food.strIngredient8}</li> <li>${food.strIngredient9}</li> <li>${food.strIngredient10}</li>')" class="food__name">${food.strMeal}</h4>
+    <img onclick="displayFood('<img src=${food.strMealThumb}><h1>${food.strMeal}</h1> <h2>Ingredinet:</h2>  <li>${food.strIngredient1}</li><li>${food.strIngredient2}</li><li>${food.strIngredient3}</li><li>${food.strIngredient4}</li><li>${food.strIngredient5}</li><li>${food.strIngredient6}</li><li>${food.strIngredient7}</li> <li>${food.strIngredient8}</li> <li>${food.strIngredient9}</li> <li>${food.strIngredient10}</li>')" src=${food.strMealThumb}>
+    <h4 onclick="displayFood('<img src=${food.strMealThumb}><h1>${food.strMeal}</h1> <h2>Ingredinet:</h2> <li>${food.strIngredient1}</li><li>${food.strIngredient2}</li><li>${food.strIngredient3}</li><li>${food.strIngredient4}</li><li>${food.strIngredient5}</li><li>${food.strIngredient6}</li><li>${food.strIngredient7}</li> <li>${food.strIngredient8}</li> <li>${food.strIngredient9}</li> <li>${food.strIngredient10}</li>')" class="food__name">${food.strMeal}</h4>
 
     `;
     mealDiv.innerHTML = mealInfo;
